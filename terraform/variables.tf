2ad61908496dbe161d308457843e156f0f21ec16
stevenISMAEL <larasteveen72@gmail.com>
2026-06-04 15:18:22 -0500

    fix: corregir nombre de imagen en deployment

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,7 +7,7 @@ variable "location" {
 variable "resource_group_name" {
   description = "Nombre del Grupo de Recursos"
   type        = string
-  default     = "rg-petclinic-devops2"
+  default     = "rg-petclinic-devops"
 }
 
 variable "aks_name" {
