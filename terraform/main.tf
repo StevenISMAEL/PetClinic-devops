8f3c41baa8ba57404f67df63a7778c2fdce21a63
stevenISMAEL <larasteveen72@gmail.com>
2026-06-06 14:53:12 -0500

    fix: reduce GKE node disk size to 50GB and use standard disk to fit within GCP quota limits

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,7 +29,9 @@ resource "google_container_node_pool" "primary" {
   node_count = 3
 
   node_config {
-    machine_type = "e2-medium" # 2 vCPU, 4GB RAM (equivalente a Standard_B2s de Azure)
+    machine_type = "e2-medium" # 2 vCPU, 4GB RAM
+    disk_size_gb = 50          # Reducimos el disco a 50GB por nodo
+    disk_type    = "pd-standard" # Usamos HDD estándar para no gastar cuota SSD
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
